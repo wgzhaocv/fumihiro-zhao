03183478d4ab7cbae058c82ef8a6e198cfa0f08f
yumemidori <wgzhaocv@gmail.com>
2023-11-21 00:50:52 +0900

    header continue

--- app/(main)/Header.tsx
+++ app/(main)/Header.tsx
@@ -9,7 +9,26 @@ import {
   useMotionValue,
 } from "framer-motion";
 import { usePathname } from "next/navigation";
-import React, { useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
+import { NavigationBar } from "./NavigationBar";
+import { ThemeSwitch } from "./ThemeSwitch";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+import { GitHubBrandIcon, GoogleBrandIcon } from "@/assets";
+import { url } from "@/lib";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipPortal,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export const Header = () => {
   const isHome = usePathname() === "/";
@@ -33,6 +52,18 @@ export const Header = () => {
     },
     []
   );
+
+  useEffect(() => {
+    const downDelay = avatarRef.current?.offsetTop ?? 0;
+    const upDelay = 64;
+    const setProperty = (prop: string, value: string | null) => {
+      document.documentElement.style.setProperty(prop, value);
+    };
+
+    const removeProperty = (prop: string) => {
+      document.documentElement.style.removeProperty(prop);
+    };
+  }, [isHome]);
 
   return (
     <>
@@ -129,10 +160,109 @@ export const Header = () => {
                   )}
                 </AnimatePresence>
               </motion.div>
+              <div className="flex flex-1 justify-end md:justify-center">
+                <NavigationBar.Desktop className="pointer-events-auto relative z-50 md:hidden" />
+                <NavigationBar.Mobile className="pointer-events-auto relative z-50 hidden md:block" />
+              </div>
+              <motion.div
+                className="flex justify-end gap-3 md:flex-1"
+                initial={{ opacity: 0, y: -20, scale: 0.95 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+              >
+                <UserInfo />
+                <div className=" pointer-events-auto">
+                  <ThemeSwitch />
+                </div>
+              </motion.div>
             </div>
           </Container>
         </div>
       </motion.header>
+      {isHome && <div className="h-[--content-offset]" />}
     </>
+  );
+};
+
+const UserInfo = () => {
+  const [tooltipOpen, setTooltipOpen] = React.useState(false);
+  const pathname = usePathname();
+  const { user } = useUser();
+  console.log(user);
+
+  const StrategyIcon = useMemo(() => {
+    const strategy = user?.primaryEmailAddress?.verification.strategy;
+    if (!strategy) return null;
+    if (strategy === "from_oauth_github") {
+      return GitHubBrandIcon;
+    } else if (strategy === "from_oauth_google") {
+      return GoogleBrandIcon;
+    }
+  }, [user?.primaryEmailAddress?.verification.strategy]);
+  return (
+    <AnimatePresence>
+      <SignedIn key="user-info">
+        <motion.div
+          className=" pointer-events-auto flex items-center relative h-10"
+          initial={{ opacity: 0, x: 25 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 25 }}
+        >
+          <UserButton
+            afterSignOutUrl={url(pathname).href}
+            appearance={{
+              elements: {
+                avatarBox: "w-9 h-9 ring-2 ring-white/20",
+              },
+            }}
+          >
+            {StrategyIcon && (
+              <span className=" pointer-events-none absolute -bottom-1 -right-1 flex h-4 w-4 select-none items-center justify-center rounded-full bg-white dark:bg-zinc-900">
+                <StrategyIcon className="w-3 h-3" />
+              </span>
+            )}
+          </UserButton>
+        </motion.div>
+      </SignedIn>
+      <SignedOut key="sign-in">
+        <motion.div
+          className=" pointer-events-auto"
+          initial={{ opacity: 0, x: 25 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 25 }}
+        >
+          <TooltipProvider disableHoverableContent>
+            <Tooltip open={tooltipOpen} onOpenChange={setTooltipOpen}>
+              <SignInButton mode="modal" redirectUrl={url(pathname).href}>
+                <TooltipTrigger asChild>
+                  <button
+                    type="button"
+                    className={clsx(
+                      "h-10 rounded-full bg-gradient-to-b from-zinc-50/50 to-white/90",
+                      "px-3 text-sm shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur",
+                      "transition dark:from-zinc-900/50 dark::to-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+                    )}
+                  ></button>
+                </TooltipTrigger>
+              </SignInButton>
+              <AnimatePresence>
+                {tooltipOpen && (
+                  <TooltipPortal forceMount>
+                    <TooltipContent asChild>
+                      <motion.div
+                        initial={{ opacity: 0, scale: 0.96 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0.96 }}
+                      >
+                        Log In
+                      </motion.div>
+                    </TooltipContent>
+                  </TooltipPortal>
+                )}
+              </AnimatePresence>
+            </Tooltip>
+          </TooltipProvider>
+        </motion.div>
+      </SignedOut>
+    </AnimatePresence>
   );
 };
--- app/(main)/NavigationBar.tsx
+++ app/(main)/NavigationBar.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { navigationItems } from "@/config/navItems";
+import { Popover, type PopoverProps, Transition } from "@headlessui/react";
 import clsx from "clsx";
-import { motion } from "framer-motion";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React, { use } from "react";
 
 type NavItemProps = {
   href: string;
@@ -26,9 +29,167 @@ const NavItem = ({ href, children }: NavItemProps) => {
       >
         {children}
         {isActive && (
-          <motion.span className=" absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-lime-700/0 via-lime-700/70 to-lime-700/0 dark:from-lime-400/0 dark:via-lime-400/40 dark:to-lime-400/0"></motion.span>
+          <motion.span
+            layoutId="active-nav-item"
+            className=" absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-lime-700/0 via-lime-700/70 to-lime-700/0 dark:from-lime-400/0 dark:via-lime-400/40 dark:to-lime-400/0"
+          ></motion.span>
         )}
       </Link>
     </li>
   );
 };
+
+const Desktop = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  const mousex = useMotionValue(0);
+  const mousey = useMotionValue(0);
+  const radius = useMotionValue(0);
+  const handleMouseMove = React.useCallback(
+    ({ clientX, clientY, currentTarget }: React.MouseEvent<HTMLDivElement>) => {
+      const { left, top, width, height } =
+        currentTarget.getBoundingClientRect();
+      mousex.set(clientX - left);
+      mousey.set(clientY - top);
+      radius.set(Math.sqrt(width ** 2 + height ** 2) / 2.5);
+    },
+    [mousex, mousey, radius]
+  );
+
+  const background = useMotionTemplate`radial-gradient(${radius}px circle at ${mousex}px ${mousey}px, var(--spotlight-color) 0%, transparent 65%)`;
+
+  return (
+    <nav
+      onMouseMove={handleMouseMove}
+      className={clsx(
+        "group relative rounded-full bg-gradient-to-b from-zinc-50/70 to-white/90",
+        "shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-md",
+        "dark:from-zinc-900/70 dark:to-zinc-800/90 dark:ring-zinc-100/10",
+        "[--spotlight-color:rgb(236_252_203_/_0.6)]  dark:[--spotlight-color:rgb(217_249_157_/_0.07)]",
+        className
+      )}
+      {...props}
+    >
+      <motion.div
+        className=" pointer-events-none absolute -inset-px rounded-full opacity-0 transition-opacity group-hover:opacity-100 duration-500"
+        style={{ background }}
+        aria-hidden="true"
+      ></motion.div>
+
+      <ul className="flex bg-transparent px-3 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+        {navigationItems.map((item) => (
+          <NavItem key={item.href} href={item.href}>
+            {item.text}
+          </NavItem>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+const MobileNavItem = ({ href, children }: NavItemProps) => {
+  return (
+    <li>
+      <Popover.Button
+        className="relative block px-3 py-2 transition"
+        as={Link}
+        href={href}
+      >
+        {children}
+      </Popover.Button>
+    </li>
+  );
+};
+
+const MobileNav = (props: PopoverProps<"div">) => {
+  return (
+    <Popover {...props}>
+      <Popover.Button className="group flex items-center rounded-full bg-gradient-to-b from-zinc-50/20 to-white/80 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur-md focus:outline-none focus-visible:ring-2 dark:from-zinc-900/30 dark:to-zinc-800/80 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20 dark:focus-visible:ring-yellow-500/80">
+        Go To
+        <svg
+          viewBox="0 0 8 6"
+          aria-hidden="true"
+          className="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400"
+        >
+          <path
+            d="M1.75 1.75 4 4.25l2.25-2.5"
+            fill="none"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </Popover.Button>
+      <Transition.Root>
+        <Transition.Child
+          as={React.Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Popover.Overlay
+            className={
+              "fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur dark:bg-black/80"
+            }
+          ></Popover.Overlay>
+        </Transition.Child>
+        <Transition.Child
+          as={React.Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            focus
+            className={
+              "fixed inset-x-4 top-8 z-50 origin-top rounded-3xl  bg-gradient-to-b from-zinc-100/75 to-white p-8 ring-1 ring-zinc-900/5 dark:from-zinc-900/50 dark:to-zinc-900 dark:ring-zinc-800"
+            }
+          >
+            <div className="flex flex-row-reverse item-center justify-between">
+              <Popover.Button aria-label="Close Menu" className={"-m-1 p-1"}>
+                <svg
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                  className="h-6 w-6 text-zinc-500 dark:text-zinc-400"
+                >
+                  <path
+                    d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </Popover.Button>
+              <h2 className="text-sm font-medium text-zinc-600 dark:text-zinc-400">
+                Menu
+              </h2>
+            </div>
+            <nav className="mt-6">
+              <ul className="-my-2 divide-y divide-zinc-500/20 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+                {navigationItems.map((item) => (
+                  <MobileNavItem key={item.href} href={item.href}>
+                    {item.text}
+                  </MobileNavItem>
+                ))}
+              </ul>
+            </nav>
+          </Popover.Panel>
+        </Transition.Child>
+      </Transition.Root>
+    </Popover>
+  );
+};
+
+export const NavigationBar = {
+  Desktop,
+  Mobile: MobileNav,
+} as const;
